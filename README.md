95d526aac85eaa6e51a5718de82ba9fec7eb00aa
Paulius Jakimavicius <ripexz@gmail.com>
2013-03-17 17:45:16 +0000

    v0.2 Nearby mine count is now calculated and shown

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Python Tkinter Minesweeper
+Python Tkinter Minesweeper v0.2
 ===========================
 
 Minesweeper game written in Python using Tkinter GUI library.
@@ -8,4 +8,14 @@ Contents:
 
 - */minesweeper.py* - The actual python program
 - */images/* - GIF Images ready for usage with Tkinter
-- */images/original* - Original PNG images made with GraphicsGale 
+- */images/original* - Original PNG images made with GraphicsGale
+
+To Do:
+----------
+- Clear all nearby tiles with 0 mine counter when a non-mine tile is clicked
+- Have specific number of mines, rather than random
+- Flag counter
+- Time counter
+- Highscore table
+- Adjustable grid and mine count
+- ...and then some...
--- minesweeper.py
+++ minesweeper.py
@@ -31,9 +31,11 @@ class Minesweeper:
         # create buttons
         self.buttons = dict({})
         mines = 0
+        x_coord = 1
+        y_coord = 0
         for x in range(0, 100):
             mine = 0
-            ### gfx reassignable for debugging purposes
+            # tile image changeable for debug reasons:
             gfx = self.tile_plain
             # currently random amount of mines
             if random.uniform(0.0, 1.0) < 0.1:
@@ -43,19 +45,111 @@ class Minesweeper:
             # 1 = if a mine y/n (1/0)
             # 2 = state (0 = unclicked, 1 = clicked, 2 = flagged)
             # 3 = button id
-            self.buttons[x] = [ Button(frame, image = gfx), mine, 0, x ]
+            # 4 = [x, y] coordinates in the grid
+            # 5 = nearby mines, 0 by default, calculated after placement in grid
+            self.buttons[x] = [ Button(frame, image = gfx),
+                                mine,
+                                0,
+                                x,
+                                [x_coord, y_coord],
+                                0 ]
             self.buttons[x][0].bind('<Button-1>', self.lclicked_wrapper(x))
             self.buttons[x][0].bind('<Button-3>', self.rclicked_wrapper(x))
+
+            # calculate coords:
+            y_coord += 1
+            if y_coord == 10:
+                y_coord = 0
+                x_coord += 1
         
         # lay buttons in grid
-        x = 1
-        y = 0
         for key in self.buttons:
-            self.buttons[key][0].grid( row = x, column = y )
-            y += 1
-            if y == 10:
-                y = 0
-                x += 1
+            self.buttons[key][0].grid( row = self.buttons[key][4][0], column = self.buttons[key][4][1] )
+
+        # find nearby mines and display number on tile
+        for key in self.buttons:
+            nearby_mines = 0
+            # if not at edges
+            if self.buttons[key][4][0] != 1 and self.buttons[key][4][0] != 10 \
+            and self.buttons[key][4][1] != 0 and self.buttons[key][4][1] != 9:
+                if self.buttons[key-9][1] == 1:     #top right
+                    nearby_mines += 1
+                if self.buttons[key-10][1] == 1:    #top middle
+                    nearby_mines += 1
+                if self.buttons[key-11][1] == 1:    #top left
+                    nearby_mines += 1
+                if self.buttons[key-1][1] == 1:     #left
+                    nearby_mines += 1
+                if self.buttons[key+1][1] == 1:     #right
+                    nearby_mines += 1
+                if self.buttons[key+9][1] == 1:     #bottom left
+                    nearby_mines += 1
+                if self.buttons[key+10][1] == 1:    #bottom middle
+                    nearby_mines += 1
+                if self.buttons[key+11][1] == 1:    #bottom right
+                    nearby_mines += 1
+
+            # if top row:
+            if self.buttons[key][4][0] == 1:
+                if self.buttons[key+10][1] == 1:    #bottom middle
+                    nearby_mines += 1
+                # if not top left
+                if self.buttons[key][4][1] != 0:
+                    if self.buttons[key-1][1] == 1:     #left
+                        nearby_mines += 1
+                    if self.buttons[key+9][1] == 1:     #bottom left
+                        nearby_mines += 1
+                # if not top right
+                if self.buttons[key][4][1] != 9:
+                    if self.buttons[key+1][1] == 1:     #right
+                        nearby_mines += 1
+                    if self.buttons[key+11][1] == 1:    #bottom right
+                        nearby_mines += 1
+            # if bottom row:
+            if self.buttons[key][4][0] == 10:
+                if self.buttons[key-10][1] == 1:    #top middle
+                    nearby_mines += 1
+                # if not bottom left
+                if self.buttons[key][4][1] != 0:
+                    if self.buttons[key-1][1] == 1:     #left
+                        nearby_mines += 1
+                    if self.buttons[key-11][1] == 1:    #top left
+                        nearby_mines += 1
+                # if not top right
+                if self.buttons[key][4][1] != 9:
+                    if self.buttons[key+1][1] == 1:     #right
+                        nearby_mines += 1
+                    if self.buttons[key-9][1] == 1:     #top right
+                        nearby_mines += 1
+            # if left side but not top or bottom:
+            if self.buttons[key][4][1] == 0 and self.buttons[key][4][0] != 1 \
+                                            and self.buttons[key][4][0] != 10:
+                if self.buttons[key-10][1] == 1:    #top middle
+                    nearby_mines += 1
+                if self.buttons[key-9][1] == 1:     #top right
+                    nearby_mines += 1
+                if self.buttons[key+1][1] == 1:     #right
+                    nearby_mines += 1
+                if self.buttons[key+11][1] == 1:    #bottom right
+                    nearby_mines += 1
+                if self.buttons[key+10][1] == 1:    #bottom middle
+                    nearby_mines += 1
+            # if right side but not top or bottom:
+            if self.buttons[key][4][1] == 9 and self.buttons[key][4][0] != 1 \
+                                            and self.buttons[key][4][0] != 10:
+                if self.buttons[key-10][1] == 1:    #top middle
+                    nearby_mines += 1
+                if self.buttons[key-11][1] == 1:    #top left
+                    nearby_mines += 1
+                if self.buttons[key-1][1] == 1:     #left
+                    nearby_mines += 1
+                if self.buttons[key+9][1] == 1:     #bottom left
+                    nearby_mines += 1
+                if self.buttons[key+10][1] == 1:    #bottom middle
+                    nearby_mines += 1
+
+            # store mine count in button data list
+            self.buttons[key][5] = nearby_mines
 
         #add mine count at the end
         self.label2 = Label(frame, text="Mines: "+str(mines))
@@ -80,7 +174,10 @@ class Minesweeper:
             self.gameover()
         else:
             #change image and state
-            button_data[0].config(image = self.tile_clicked)
+            if button_data[5] == 0:
+                button_data[0].config(image = self.tile_clicked)
+            else:
+                button_data[0].config(image = self.tile_no[button_data[5]-1])
             button_data[2] = 1
 
     def rclicked(self, button_data):
